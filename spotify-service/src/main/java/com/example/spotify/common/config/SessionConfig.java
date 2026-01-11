4cae2108f60cf81985b16df826080d4c1ad3350c
MarceloMMendes <marcelo.mendes@maxmilhas.com.br>
2026-01-11 17:20:58 -0300

    feat: add JSON serializer for Redis sessions

--- spotify-service/src/main/java/com/example/spotify/common/config/SessionConfig.java
+++ spotify-service/src/main/java/com/example/spotify/common/config/SessionConfig.java
@@ -2,6 +2,8 @@ package com.example.spotify.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
@@ -17,5 +19,10 @@ public class SessionConfig {
         serializer.setUseHttpOnlyCookie(true);
         return serializer;
     }
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+          return new GenericJackson2JsonRedisSerializer();
+      }
+
 
 }
